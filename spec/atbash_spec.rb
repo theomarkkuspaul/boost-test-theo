81e24c4d075f41460e2b075d643dd1f3d7ce475d
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2017-05-23 11:12:17 +1200

    Add Atbash tests to confirm functional decryption algorithm

--- spec/atbash_spec.rb
+++ spec/atbash_spec.rb
@@ -7,3 +7,46 @@ RSpec.describe Atbash do
   end
 
 end
+
+RSpec.describe Atbash, "#decrypt" do
+
+  context 'provided this cipher: \'zodvqukgwefbyitmrsplhacxnj\'' do
+
+    it 'should decrypt \'dzs\' into understandable english' do
+
+      cipher = Atbash.new('zodvqukgwefbyitmrsplhacxnj')
+      encrypted = 'dzs'
+
+      expect(cipher.decrypt(encrypted)).to eq('car')
+
+    end
+
+  end
+
+  context 'provided this cipher: \'xipmuzfkbrlwotjancqgveshdy\'' do
+
+    it 'should decrypt \'skd qj qucbjvq?\' into understandable english' do
+
+      cipher = Atbash.new('xipmuzfkbrlwotjancqgveshdy')
+      encrypted = 'skd qj qucbjvq?'
+
+      expect(cipher.decrypt(encrypted)).to eq('why so serious?')
+
+    end
+
+  end
+
+  context 'provided this cipher: \'oephjizkxdawubnytvfglqsrcm\'' do
+
+    it 'should decrypt \'knlfgnb, sj koqj o yvnewju\' into understandable english' do
+
+      cipher = Atbash.new('oephjizkxdawubnytvfglqsrcm')
+      encrypted = 'knlfgnb, sj koqj o yvnewju'
+
+      expect(cipher.decrypt(encrypted)).to eq('houston, we have a problem')
+
+    end
+
+  end
+
+end
