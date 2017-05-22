a5c24fcd5279d434437ab62425a76f1bf24a9374
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2017-05-23 11:11:37 +1200

    Add driver code

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative 'atbash.rb'
+
+# DRIVER CODE
+cipher = 'oephjizkxdawubnytvfglqsrcm'
+atbash = Atbash.new(cipher)
+
+p atbash.decrypt('knlfgnb, sj koqj o yvnewju')
+
+# houston we have a problem
